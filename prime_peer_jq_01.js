71f9208e319b714bcfd3f8627148360695341b74
mliq <liquori@gmail.com>
2015-04-20 13:43:09 -0500

    initial commit

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -1,5 +1,32 @@
 //Utility function
+
+function Person(age, sex, weight){
+	this.age = age;
+	this.sex = sex;
+	this.weight = weight;
+
+	if(sex == 1) {
+		this.sex = "Male";
+	}else{
+		this.sex = "Female";
+	}
+}
+
 //A simple random number generator
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+var i = 0;
+var peopleArray = [];
+
+// jQuery
+$(document).ready(function(){
+  $("#someButton").on("click", function(){
+    // Create person on button click
+    peopleArray[i] = new Person(randomNumber(1,100), randomNumber(1,2), randomNumber(1,100));
+    // Append a person
+    $("#people").prepend("<p>Person" + i + ": " + " age: " + peopleArray[i].age + " sex: " + peopleArray[i].sex + " weight: " + peopleArray[i].weight + "</p>");
+    i++;
+  });
+});
